2f6e9bd2ab34627381bcd86e7e281b26bc69bddb
Jacob Keisling <contact@keisling.me>
2024-06-21 23:42:36 -0500

    Fix GH Pages deployments

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,22 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import Pages from "vite-plugin-pages";
+import getRepoName from "git-repo-name";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), Pages()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+export default defineConfig(({ command, mode }) => {
+    const isGitHubPages = mode === 'github-pages'
+    const base = isGitHubPages ? `/${getRepoName.sync()}/` : '/'
+  
+    return {
+      plugins: [react(), Pages()],
+      base: base,
+      build: {
+        outDir: isGitHubPages ? 'dist-github' : 'dist'
+      },
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+    }
+  })
